a99d6d73236e1a9d49e8f8b3310da6d675a920f9
Rafael Alfaro <rafaela@thehangar.cr>
2018-09-30 16:04:53 -0600

    Adds const array example

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/********* CONST *********/
+/****************************/
+/********* CONST #1 *********/
 const hello = 'Hello World'
 console.log(hello);
 try {
@@ -8,4 +9,20 @@ hello = 'Hello World #2' //should throw an error const can only be assigned once
 }
 console.log(hello);
 console.log('\nThe End.');
-/*************************/
+/****************************/
+/********* CONST #2 *********/
+const array_1 = [1,2];
+console.log(array_1);
+try {
+  array_1 = [1,2,3]; //should throw an error const can olny be assigned once
+} catch (error) {
+  console.error(error.message);
+}
+console.log(array_1);
+try {
+  array_1.push(3); //should not thow an error only assignment is protected for overwrites on const variables
+} catch (error) {
+  console.log(error.message);
+}
+console.log(array_1);
+/****************************/
